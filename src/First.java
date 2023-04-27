e0580dda93f64cb31a6613933f0803ee142199fd
ambrishba-sacumen <ambrish.ba@sacumen.com>
2023-04-27 17:56:06 +0530

    first commit

---
+++ src/First.java
@@ -0,0 +1,21 @@
+import  java.util.*;
+
+public class First {
+    public static void list()
+    {
+        ArrayList<String> a=new ArrayList<>();
+        a.add("java");
+        a.add("developer");
+        a.add("Cyber Security");
+       a.add("Cyber ");
+        a.add("Security");
+        a.add("Git hub");
+      for(String s:a)
+      {
+          System.out.println(s);
+      }
+
+    }
+
+
+}
